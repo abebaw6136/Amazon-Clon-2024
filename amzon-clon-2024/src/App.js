431a20f3515a70296d2f6ab65d32c5c028c55c73
abebaw6136 <abebaw1616@gmail.com>
2024-10-20 22:24:43 +0300

    Header

---
+++ amzon-clon-2024/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Header from './Componets/Header/Header';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      
+    </div>
+  );
+}
+
+export default App;
